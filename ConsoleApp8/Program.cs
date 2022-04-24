8ec89a332eabe7d13e794e5bbd56576e9a3180b3
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2022-04-24 12:26:46 +0400

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.Json;
+
+namespace MyApp 
+{
+    class Users
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        Users(int id,string name,string email,string password)
+        {
+            Id = id;Name = name;Email = email;Password = password;
+        }
+        Users() { }
+
+    }
+
+   class User : Users
+   {
+ 
+
+   }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            User[] username = { 
+                new User{ Name = "Nihad",password="Nihad1368"}
+            };
+            var json = JsonSerializer.Serialize(username);
+            File.WriteAllText("file.txt", json);
+            var text = File.ReadAllText("file.txt");
+            User[]? user = JsonSerializer.Deserialize<User[]>(text);
+            foreach (User item in user)
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.password);
+            }
+        }
+    }
+}
