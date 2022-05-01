cfa4530e1721963098dcc5aff3b9a9bae4d11e3e
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2022-05-01 12:35:01 +0400

    0.7

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -14,8 +14,7 @@ namespace MyApp
                 Console.WriteLine("1) Admin 2)User");
                 var select = Convert.ToInt32(Console.ReadLine());
                 if (select == 2)
-                {
-                    
+                {   
                     Users user = new Users();
                     user.select();
                 }
@@ -24,28 +23,6 @@ namespace MyApp
                 }
                 else continue;
             }
-            //Users anmes = new Users();
-            //anmes.select();
-            //anms.copyfile("file.txt", "copyfile.txt");
-            //Admin.Admin[] admin = {
-            //    new Admin.Admin( 2,"Nicat","a.gmail.com","Nicat1369")
-            //};
-            //var json = JsonSerializer.Serialize(admin);
-            //File.AppendAllText("admin.txt", json + '\n');
-            //string[] text = File.ReadAllLines("file.txt");
-            //foreach (var item in text)
-            //{
-            //    Users []?user = JsonSerializer.Deserialize<Users[]>(item);
-            //    foreach (var item2 in user)
-            //    {
-            //        if (item2.Name == "Nihad")
-            //        {
-            //            Console.WriteLine("Tapildi");
-            //        }
-            //        else continue;
-            //    }
-            //}
-
         }
     }
 }
